9893c946ad0211e1cfb93ffd76d096f7dd42b9f8
hh <betabeeversion@gmail.com>
2019-04-08 14:30:11 +0100

    il y'a ajout d'une ligne de commentaire pour tester le commit

--- app/src/main/java/com/christ/myapplicationgithubtest/MainActivity.java
+++ app/src/main/java/com/christ/myapplicationgithubtest/MainActivity.java
@@ -2,14 +2,16 @@ package com.christ.myapplicationgithubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn = findViewById(R.id.btn);
         // ajout de commentaire pour tester
     }
 }
